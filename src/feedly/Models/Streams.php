a9cb3d9a6a511bd28d2742bf7e24c0b35411a182
Kostas Bariotis <konmpar@gmail.com>
2014-12-11 21:05:01 +0200

    proper indetation

--- src/feedly/Models/Streams.php
+++ src/feedly/Models/Streams.php
@@ -2,16 +2,20 @@
 
 namespace feedly\Models;
 
-class Streams extends FeedlyModel{
+class Streams extends FeedlyModel
+{
 
-    public function __construct($token) {
+    public function __construct($token)
+    {
         parent::__construct($token);
 
         $this->setEndpoint('/v3/streams');
     }
 
-    public function get($ids,$data = "ids") {
-        return $this->getClient()->get($this->getApiBaseUrl() . $this->getEndpoint() . '/' .$data.'?streamId='.urlencode($ids));
+    public function get($ids, $data = "ids")
+    {
+        return $this->getClient()
+                    ->get($this->getApiBaseUrl() . $this->getEndpoint() . '/' . $data . '?streamId=' . urlencode($ids));
     }
 
 }
